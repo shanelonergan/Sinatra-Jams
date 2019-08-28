c89727b150fccad99dceb34edb98d793fe3f920d
github username <github email address>
2019-08-28 17:47:22 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,37 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  def artists
+      @artists = Artist.all
+  end
+
   get "/" do
     erb :welcome
   end
+
+  get '/artists' do
+    if params[:search_name]
+      @artists = artists.where("name LIKE ?", "%#{params[:search_name]}%")
+    else
+      @artists = artists
+    end
+    erb :index
+  end
+
+  get '/artists/new' do
+    erb :new
+  end
+
+  get '/artists/:id' do
+    @artist = artists.find(params[:id])
+    erb :show
+  end
+
+  post "/artists" do
+
+    @artist = Artist.create(params[:artist])
+    redirect "/artists/#{@artist.id}"
+  end
+
 
 end
